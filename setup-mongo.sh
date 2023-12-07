3c2f29acb05f50b6e04dcb0c0318fe8125b907ef
nmserver <nmserver@nmservers-MBP.app.byu.edu>
2023-12-07 12:57:04 -0700

    changed host to localhost and removed --bind_ip_all option

--- setup-mongo.sh
+++ setup-mongo.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # Predefined MongoDB credentials
-DB_USER=battle
-DB_PASS=studies
+MONGO_INITDB_ROOT_USERNAME=battle
+MONGO_INITDB_ROOT_PASSWORD=studies
 
 # Run mongod to initialize MongoDB
-mongod --fork --logpath /var/log/mongod.log --bind_ip_all --auth
+mongod --fork --logpath /var/log/mongod.log --auth
 
 # Create a MongoDB user with the predefined password
-mongo admin --eval "db.createUser({ user: '$DB_USER', pwd: '$DB_PASS', roles: ['root'] })"
+mongo admin --eval "db.createUser({ user: '$MONGO_INITDB_ROOT_USERNAME', pwd: '$MONGO_INITDB_ROOT_PASSWORD', roles: ['root'] })"
 
 # Stop the temporary mongod process
 mongod --shutdown
 
 # Run mongod with authentication enabled
-mongod --bind_ip_all --auth &
+mongod --auth &
 
 # Wait for MongoDB to start
 sleep 10
 
 # Run mongorestore on the JSON/BSON files in /data/db/dump
-mongorestore --host localhost --port 27017 --authenticationDatabase admin -u $DB_USER -p $DB_PASS --db admin 
-/data/db/dump
-mongorestore --host localhost --port 27017 --authenticationDatabase admin -u $DB_USER -p $DB_PASS --db Discography 
-/data/db/dump
+mongorestore --host localhost --port 27017 --authenticationDatabase admin -u $MONGO_INITDB_ROOT_USERNAME -p 
+$MONGO_INITDB_ROOT_PASSWORD --db admin /data/db/dump
+mongorestore --host localhost --port 27017 --authenticationDatabase admin -u $MONGO_INITDB_ROOT_USERNAME -p 
+$MONGO_INITDB_ROOT_PASSWORD --db Discography /data/db/dump
 
 # Keep the container running
 tail -f /dev/null
