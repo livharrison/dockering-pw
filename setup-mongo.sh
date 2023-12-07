18f6c204484a204740e761aa5fe0447e0400d1a7
nmserver <nmserver@nmservers-MBP.app.byu.edu>
2023-12-07 13:43:53 -0700

    mongo container can run & restore on its own

--- setup-mongo.sh
+++ setup-mongo.sh
@@ -9,7 +9,7 @@ mongod --logpath /var/log/mongod.log \
     --auth \
     --bind_ip 127.0.0.1 --port 27017 \
     --fork
-
+echo "Started mongo"
 # Create a MongoDB user with the predefined password
 mongo admin --eval "db.createUser({ user: '$MONGO_INITDB_ROOT_USERNAME', pwd: '$MONGO_INITDB_ROOT_PASSWORD', roles: ['root'] })"
 
@@ -21,12 +21,14 @@ mongod --auth &
 
 # Wait for MongoDB to start
 sleep 10
-
+echo "running mongorestore"
 # Run mongorestore on the JSON/BSON files in /data/db/dump
 mongorestore --host 127.0.0.1 --port 27017 --authenticationDatabase admin \
-    -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --db admin /data/db/dump
+    -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --db admin /data/db/dump/admin
 mongorestore --host 127.0.0.1 --port 27017 --authenticationDatabase admin \
-    -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --db Discography /data/db/dump
+    -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --db Discography /data/db/dump/Discography
+
+echo "finished mongorestore"
 
 # Keep the container running
 tail -f /dev/null
